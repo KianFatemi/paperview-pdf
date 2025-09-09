6d6835a05d3de993c50e17658ff32c9b5f730657
KianFatemi <kianfatemi044@gmail.com>
2025-09-09 12:36:01 -0400

    initial electron setup

---
+++ electron/main.ts
@@ -0,0 +1,59 @@
+// electron/main.ts
+
+import { app, BrowserWindow, ipcMain, dialog } from 'electron';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url'; 
+import fs from 'node:fs/promises';
+
+const __filename = fileURLToPath(import.meta.url); 
+const __dirname = path.dirname(__filename); 
+
+
+process.env.DIST = path.join(__dirname, '../dist'); // <-- This now works
+process.env.VITE_PUBLIC = app.isPackaged ? process.env.DIST : path.join(process.env.DIST, '../public');
+
+let win: BrowserWindow | null;
+
+const VITE_DEV_SERVER_URL = process.env['VITE_DEV_SERVER_URL'];
+
+function createWindow() {
+  win = new BrowserWindow({
+    icon: path.join(process.env.VITE_PUBLIC ?? '', 'electron-vite.svg'),    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'), // <-- This now works
+    },
+  });
+
+  // Test active push message to Renderer-process.
+  win.webContents.on('did-finish-load', () => {
+    win?.webContents.send('main-process-message', (new Date).toLocaleString());
+  });
+
+  if (VITE_DEV_SERVER_URL) {
+    win.loadURL(VITE_DEV_SERVER_URL);
+  } else {
+    // win.loadFile('dist/index.html')
+    win.loadFile(path.join(process.env.DIST ?? '', 'index.html'));  }
+}
+
+// Handle opening a file
+ipcMain.handle('dialog:openFile', async () => {
+  const { canceled, filePaths } = await dialog.showOpenDialog({
+    properties: ['openFile'],
+    filters: [{ name: 'PDFs', extensions: ['pdf'] }]
+  });
+  if (!canceled && filePaths.length > 0) {
+    const filePath = filePaths[0];
+    const fileData = await fs.readFile(filePath);
+    return new Uint8Array(fileData);
+  }
+  return null;
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+    win = null;
+  }
+});
+
+app.whenReady().then(createWindow);
