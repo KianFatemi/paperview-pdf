f5a9206fdd430c6f32f53ea3ab24f5fdbf84d75c
KianFatemi <kianfatemi044@gmail.com>
2025-10-10 15:25:30 -0400

    updated sidebar to remove thumbnail label

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -10,9 +10,6 @@ interface SidebarProps {
 const Sidebar: React.FC<SidebarProps> = ({ pdfData, activePage, setActivePage }) => {
   return (
     <aside className="w-64 bg-gray-800 shadow-md flex flex-col">
-      <div className="p-4 border-b border-gray-700">
-        <h2 className="text-lg font-semibold">Thumbnails</h2>
-      </div>
       <ThumbnailView
         pdfData={pdfData}
         activePage={activePage}
